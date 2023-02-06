0d4749b8d9e579de0f1f262bdfac7bd7e4195653
Dani <daniel.varlan20@gmail.com>
2023-02-06 17:57:08 +0100

    Added Background Toggle

--- src/components/ClockApp.vue
+++ src/components/ClockApp.vue
@@ -12,22 +12,10 @@
 <style scoped>
 .clock {
     font-size: 90px;
-    /*background: linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0));
-    backdrop-filter: blur(0.4px);
-    border-radius: 10%;
-    box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
-    width: max-content;
-    margin: auto;*/
     color: rgba(181, 215, 246, 0.597);
 }
 .date {
     font-size: 50px;
-    /*background: linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0));
-    backdrop-filter: blur(0.4px);
-    border-radius: 10%;
-    box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
-    width: max-content;
-    margin: auto;*/
     color: rgba(181, 215, 246, 0.597);
 }
 .blur {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
-    <!--<img alt="Vue logo" src="../assets/logo.png"> -->
+    <button id="toggleBtn" @click="toggleBackground">Toggle Animation</button>
     <ClockApp />
     <WeatherPanel />
-    <ParticleBackgrund />
+    <ParticleBackgrund v-if="enableAnimation" />
+    <div class="background" v-else></div>
   </div>
 </template>
 
@@ -19,6 +20,36 @@ export default {
     ParticleBackgrund,
     ClockApp,
     WeatherPanel
+  },
+  data() {
+    return {
+      enableAnimation: true
+    }
+  },
+  methods: {
+    toggleBackground() {
+      this.enableAnimation = !this.enableAnimation
+    }
   }
 }
 </script>
+
+<style>
+.background {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  z-index: -100;
+  background-color: #1E1E1E;
+}
+
+#toggleBtn {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-top: 10px;
+  margin-right: 10px;
+}
+</style>
